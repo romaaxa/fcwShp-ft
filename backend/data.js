72169f673d6c012df42ed56105ec8920331e9407
Anton <a.romashko.work@mail.ru>
2020-11-07 18:03:23 +0300

    server backend node.js

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+export default {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '../images/slimshirt.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 9,
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '../images/slimshirt.jpg',
+        price: 50,
+        brand: 'Nikeee',
+        rating: 4.2,
+        numReviews: 10,
+    },
+    {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '../images/slimshirt.jpg',
+        price: 70,
+        brand: 'Nikew',
+        rating: 4.3,
+        numReviews: 11,
+    },
+    {
+        _id: '4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '../images/slimshirt.jpg',
+        price: 90,
+        brand: 'Nikee',
+        rating: 4.7,
+        numReviews: 13,
+    },
+    ]
+}
