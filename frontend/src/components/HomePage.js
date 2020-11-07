7df388efdcb2b23c2bcdd2fa9a2dcf852b1ff41c
Anton <a.romashko.work@mail.ru>
2020-11-07 22:55:19 +0300

    [Axios] Fetch server data

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -26,6 +26,7 @@ const HomePage = () => {
                             <Link to={"/product/" + product._id}>
                                 <img className="product-image" src={product.image} alt="product" />
                             </Link>
+
                             <div className="product-name">
                                 <Link to={"/product/" + product._id}>{product.name}</Link>
                             </div>
