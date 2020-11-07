a9d9e54e1e7109fbc34b0dd9a4cfc665feb0719a
Anton <a.romashko.work@mail.ru>
2020-11-07 15:56:40 +0300

    rendering jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 const openMenu = () => {
   document.querySelector(".sidebar").classList.add("open");
@@ -46,101 +47,26 @@ const App = () => {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
+            {
+              data.products.map(product =>
+                <li>
+                  <div className="product">
+                    <img className="product-image" src={product.image} alt="product" />
 
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
 
-                <div className="product-brand">Nike</div>
+                    <div className="product-brand">{product.brand}</div>
 
-                <div className="product-price">$60</div>
+                    <div className="product-price">$ {product.price}</div>
 
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
+                    <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+                  </div>
+                </li>
+              )
+            }
 
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
-
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-
-                <div className="product-brand">Nike</div>
-
-                <div className="product-price">$60</div>
-
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
-
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-
-                <div className="product-brand">Nike</div>
-
-                <div className="product-price">$60</div>
-
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
-
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-
-                <div className="product-brand">Nike</div>
-
-                <div className="product-price">$60</div>
-
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
-
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-
-                <div className="product-brand">Nike</div>
-
-                <div className="product-price">$60</div>
-
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img className="product-image" src="./images/slimshirt.jpg" alt="product" />
-
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-
-                <div className="product-brand">Nike</div>
-
-                <div className="product-price">$60</div>
-
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
 
           </ul>
         </div>
---
+++ frontend/src/data.js
@@ -0,0 +1,30 @@
+export default {
+    products: [{
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: './images/slimshirt.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: './images/slimshirt.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        name: 'Best Pants',
+        category: 'Pants',
+        image: './images/slimshirt.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+    },
+    ]
+}
