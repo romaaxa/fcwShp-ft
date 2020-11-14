a437a82aaf376c81e634e7115023947c975a780c
Anton <a.romashko.work@mail.ru>
2020-11-14 16:07:11 +0300

    [routes] productRoute

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import Product from '../models/productModel';
+import { getToken, isAuth } from '../util';
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+});
+
+router.post("/", async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        brand: req.body.brand,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        description: req.body.description,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+    });
+
+    const newProduct = await product.save();
+
+    if (newProduct) {
+        res.status(201).send({ msg: "New Product Created!", data: newProduct });
+    }
+    return res.status(500).send({ msg: "Error in creating product!" });
+})
+
+export default router;
+
