02f2629698cd23ded88b194ce300bfbbc9941b90
Anton <a.romashko.work@mail.ru>
2020-11-21 15:36:03 +0300

    [Pages] Add Shipping page

--- frontend/src/screenPages/ShippingPage.js
+++ frontend/src/screenPages/ShippingPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShipping } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingPage = (props) => {
     const [address, setAddress] = useState('');
@@ -13,58 +14,62 @@ const ShippingPage = (props) => {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(saveShipping(address, city, postalCode, country));
+        dispatch(saveShipping({ address, city, postalCode, country }));
+        props.history.push('payment');
     }
 
     return (
-        <div className="form">
-            <form onSubmit={submitHandler}>
-                <ul className="form-container">
-                    <li>
-                        <h3>Shipping</h3>
-                    </li>
+        <div>
+            <CheckoutSteps step1 step2></CheckoutSteps>
+            <div className="form">
+                <form onSubmit={submitHandler}>
+                    <ul className="form-container">
+                        <li>
+                            <h3>Shipping</h3>
+                        </li>
 
-                    <li>
-                        <label htmlFor="address">
-                            Address
+                        <li>
+                            <label htmlFor="address">
+                                Address
                         </label>
 
-                        <input type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}>
-                        </input>
-                    </li>
+                            <input type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}>
+                            </input>
+                        </li>
 
-                    <li>
-                        <label htmlFor="city">
-                            City
+                        <li>
+                            <label htmlFor="city">
+                                City
                         </label>
 
-                        <input type="text" name="city" id="city" onChange={(e) => setCity(e.target.value)}>
-                        </input>
-                    </li>
+                            <input type="text" name="city" id="city" onChange={(e) => setCity(e.target.value)}>
+                            </input>
+                        </li>
 
-                    <li>
-                        <label htmlFor="postalCode">
-                            Postal Code
+                        <li>
+                            <label htmlFor="postalCode">
+                                Postal Code
                         </label>
 
-                        <input type="text" name="postalCode" id="postalCode" onChange={(e) => setPostalCode(e.target.value)}>
-                        </input>
-                    </li>
+                            <input type="text" name="postalCode" id="postalCode" onChange={(e) => setPostalCode(e.target.value)}>
+                            </input>
+                        </li>
 
-                    <li>
-                        <label htmlFor="country">
-                            Country
+                        <li>
+                            <label htmlFor="country">
+                                Country
                         </label>
 
-                        <input type="text" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
-                        </input>
-                    </li>
+                            <input type="text" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
+                            </input>
+                        </li>
 
-                    <li>
-                        <button type="submit" className="button-primary">Continue</button>
-                    </li>
-                </ul>
-            </form>
+                        <li>
+                            <button type="submit" className="button-primary">Continue</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
         </div>
     )
 }
